f3202d8ced70b11a7acd07b25ce35664e7758f3e
BigilAnkit <109574341+BigilAnkit@users.noreply.github.com>
2022-11-21 23:37:07 +0530

    Add files via upload

---
+++ feettocm.js
@@ -0,0 +1,23 @@
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    //grab content form input
+    var feet = document.querySelector('#feet');
+    var results = document.querySelector('#results');
+
+    e.preventDefault();
+    feet = parseInt(feet.value);
+
+    if(isNaN(feet))
+    results.textContent = "Please enter a valid number! ";
+
+    else if(feet <= 0)
+    results.textContent = "Please enter a feet value > 0 "
+
+    else{
+        var converted_value = feet * 30.48;
+        results.textContent = `${converted_value} cm`;
+        document.querySelector('#feet').value = '';
+        document.querySelector('#inches').value = '';
+    }
+})
